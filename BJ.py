2da07a3cb2b6820e3f2b58be9bdd1d7cec0c8c56
yattinda <yattinda@gmail.com>
2019-10-24 20:43:58 +0900

    vector

--- BJ.py
+++ BJ.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 white = 1
 black =-1
 tablesize = 8
@@ -16,8 +14,6 @@ class board():
 
 #print(*board().cell, sep= "\n")
 
-class putboard():
-
     def putreversi(self, x, y, player):
 
         if self.cell[y][x] is not 0:
@@ -25,4 +21,16 @@ class putboard():
 
         else:
             self.cell[y][x] = player
-            return True
+        return True
+
+    def available(self, x ,y ,player):
+        #for xx in range(-tablesize,tablesize):
+        #    for yy in range(-tablesize,tablesize):
+                if 0 <= x <= tablesize  and 0 <= y <= tablesize:
+                    continue
+
+                distance = 0
+                while:
+                    distance += 1
+
+                    disx = x + ()
